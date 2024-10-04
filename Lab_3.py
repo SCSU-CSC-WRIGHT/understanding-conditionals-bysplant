14fe4cd745896691c175cf3974c8e95d7ed4e712
Betsy S. <sanz.betsy@gmail.com>
2024-10-04 10:08:57 -0400

    Update Lab_3.py

--- Lab_3.py
+++ Lab_3.py
@@ -1,69 +1,67 @@
 # Temperature Check
-temp_c = int(input("Enter the current temperature in Celsius."))
+temp_c = int(input("Enter the current temperature in Celsius "))
 if (temp_c > 30):
     print("It's a hot day.")
 else:
     print("The weather is cool.")
 
 # Number Comparison
-first_num = int(input("Enter one number."))
-second_num = int(input("Enter second number."))
-if (first_num > second_num)
-    print(first_num)
-elif (second_num > first_num)
-    print(second_num)
-else: print("They're equal.")
+first_num = int(input("Enter one number "))
+second_num = int(input("Enter second number "))
+if (first_num > second_num) :
+    print(first_num, "is greater than", second_num)
+elif (first_num < second_num):
+    print(second_num, "is greater than", first_num)
+else: 
+    print("They're equal.")
 
 # Password Checker
-admin_pw = yaycats!
-user_pw = input("Enter a password.")
-if (admin_pw == user_pw)
+admin_pw = "yaycats"
+user_pw = input("Enter your password ")
+if (admin_pw == user_pw):
     print("Access granted.")
-if (admin_pw != user_pw)
-    print("Acess denied.")
+if (admin_pw != user_pw):
+    print("Access denied.")
 
 # Simple Calculator
-x = float(input("Enter one number."))
-y = float(input("Enter second number."))
-op = input("Enter an operation.")
-    if (op == "+")
-        print(x + y)
-    elif (op == "-")
-        print (x - y)
-    elif (op == "*")
-        print(x * y)
-    elif (op == "/")
-        print (x / y)
+x = float(input("Enter one number "))
+y = float(input("Enter second number "))
+op = input("Enter an operation ")
+if (op == "+"):
+    print(x + y)
+elif (op == "-"):
+    print(x - y)
+elif (op == "*"):
+    print(x * y)
+elif (op == "/"):
+    print(x / y)
+else:
+    print("You didn't enter an operation")
 
 # Positive, Negative, or Zero
-num = int(input("Enter a number.")) 
-    if (num > 0)
-        print("Your number is positive.")
-    elif (num == 0)
-        print("Your number is zero.")
-    elif (num < 0)
-        print("Your number is negative.")
+num = int(input("Enter a number ")) 
+if (num > 0):
+    print("Your number is positive.")
+elif (num == 0):
+    print("Your number is zero.")
+elif (num < 0):
+    print("Your number is negative.")
 
 # Day of the Week
-1 = Monday
-2 = Tuesday
-3 = Wednesday
-4 = Thursday
-5 = Friday
-6 = Saturday
-7 = Sunday
-num = int(input("Enter a number between 1 and 7."))
-    if (num == 1)
-        print("Monday")
-    elif (num == 2)
-        print("Tuesday")
-    elif (num == 3)
-        print("Wednesday")
-    elif (num == 4)
-        print("Thursday")
-    elif (num == 5)
-        print("Friday")
-    elif (num == 6)
-        print("Saturday")
-    elif (num == 7)
-        print("Sunday")
+num = int(input("Enter a number between 1 and 7 "))
+if (num == 1):
+    print("Monday")
+elif (num == 2):
+    print("Tuesday")
+elif (num == 3):
+    print("Wednesday")
+elif (num == 4):
+    print("Thursday")
+elif (num == 5):
+    print("Friday")
+elif (num == 6):
+    print("Saturday")
+elif (num == 7):
+    print("Sunday")
+else:
+    print("You didn't enter a number between 1 and 7.")"Sunday")
